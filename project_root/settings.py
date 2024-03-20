d8803dd84374428a473fc9e4f0a1ad875b87b555
NaiNaniNai <nail02-02@mail.ru>
2024-03-20 20:17:14 +0300

    Add send email

--- project_root/settings.py
+++ project_root/settings.py
@@ -108,3 +108,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MIGRATION_LINTER_OPTIONS = {
     "no_cache": True,
 }
+
+
+# Email smtp setting
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_USE_SSL = True
