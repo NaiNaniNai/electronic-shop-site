21e100a3152eb6c2925c50820727aaf5ceea57e1
NaiNaniNai <nail02-02@mail.ru>
2024-03-21 21:46:20 +0300

    Add image field in product model, install Pillow and add model in admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
-# Register your models here.
+from shop.models import Category, Characteristic, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Model of category in admin panel"""
+
+    list_display = ("name", "parent")
+    list_display_links = ("name",)
+    ordering = ("id",)
+
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Characteristic)
+class CharacteristicAdmin(admin.ModelAdmin):
+    """Model of characteristic in admin panel"""
+
+    list_display = ("name", "value")
+    list_display_links = ("name",)
+    ordering = ("id",)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Model of product in admin panel"""
+
+    list_display = (
+        "name",
+        "color",
+        "price",
+    )
+    list_display_links = ("name",)
+    list_filter = ("category",)
+    search_fields = ("name", "category__name")
+    ordering = ("id",)
+
+    prepopulated_fields = {"slug": ("name",)}
